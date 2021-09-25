84d8ede907427d06451a4679f631066bffcc4cf2
mostafa egbarea <mostafaigbarea@gmail.com>
2021-09-25 18:09:12 +0300

    finished

--- js/app.js
+++ js/app.js
@@ -1,46 +1,33 @@
 let ul=document.getElementById("navbar__list");   
 
-/// li elements ///
-let li1=document.createElement("li");   
-let li2=document.createElement("li");
-let li3=document.createElement("li");
-let li4=document.createElement("li");
+const sections2 = Array.from(document.getElementsByTagName("section"));
 
-li1.id="section11"
-li2.id="section22"
-li3.id="section33"
-li4.id="section44"
+let counter=1;
+for(section of sections2){
+    const listItem = document.createElement('li');
+    listItem.id=`section${counter}${counter}`;
+    listItem.classList.add("nav-li");
+    const a=document.createElement("a");
+    a.href=`#section${counter}`;
+    a.classList.add("nav-a");
+    a.textContent=`section${counter}`
+    listItem.appendChild(a);
+    listItem.addEventListener("click",(e)=>{
+        let section1=document.getElementById(e.target.id.slice(0,e.target.id.length-1));
+        console.log(section1);
+        section1.scrollIntoView(true);
+    })
+    ul.appendChild(listItem);
+    counter++;
+  }
 
-/// anchor elements ///
-let a1=document.createElement("a");
-let a2=document.createElement("a");
-let a3=document.createElement("a");
-let a4=document.createElement("a");
 
-a1.href="#section1";
-a2.href="#section2";
-a3.href="#section3";
-a4.href="#section4";
-
-a1.textContent="section1";
-a2.textContent="section2";
-a3.textContent="section3";
-a4.textContent="section4";
-
-li1.appendChild(a1);
-li2.appendChild(a2);
-li3.appendChild(a3);
-li4.appendChild(a4);
-
-ul.appendChild(li1);
-ul.appendChild(li2);
-ul.appendChild(li3);
-ul.appendChild(li4);
 
 
 //////// show active nav items //////
-const litems=[li1,li2,li3,li4];
-const aitems=[a1,a2,a3,a4];
+const litems=Array.from(document.getElementsByClassName("nav-li"));
+const aitems=Array.from(document.getElementsByClassName("nav-a"));
+console.log(aitems);
 
 const sections = document.querySelectorAll("section");
 window.onscroll = () => {
@@ -57,9 +44,9 @@ window.onscroll = () => {
           li.classList.add("active");
     }
     aitems.forEach((a) => {
-        a.classList.remove("active2");
+        a.classList.remove("active");
         if (a.textContent===current) {
-              a.classList.add("active2");
+              a.classList.add("active");
         }
       });
   });
